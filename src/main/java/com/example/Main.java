543f231e5b8b9a6153ea3bf656fb86a4be4a76d8
Fedir <davidenkofedya536@gmail.com>
2026-04-13 12:46:02 +0300

    main file

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,4 +5,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-}}
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Employee> list = new ArrayList<>();
+
+        System.out.print("Введіть кількість співробітників: ");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Введення даних для співробітника #" + (i + 1) + ":");
+
+            System.out.print("Ім'я: ");
+            String n = scanner.nextLine();
+
+            System.out.print("Посада: ");
+            String p = scanner.nextLine();
+
+            System.out.print("Зарплата: ");
+            double s = scanner.nextDouble();
+            scanner.nextLine();
+            list.add(new Employee(n, p, s));
+        }
+        System.out.println("\nСписок всіх співробітників:");
+        for (Employee emp : list) {
+            System.out.println(emp);
+        }
+    }
+}
